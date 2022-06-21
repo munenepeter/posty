7b4a3c6ce8ef32ae0621759b39df5fcf6d440080
munenepeter <peternjega7@gmail.com>
2022-06-21 15:10:12 +0300

    post belongs to a user

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,13 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Post extends Model
-{
+class Post extends Model {
     use HasFactory;
 
     protected $fillable = [
-       'title',
-       'body',
-       'user_id'
-    ]; 
+        'title',
+        'body',
+        'user_id'
+    ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
