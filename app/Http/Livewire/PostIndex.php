238dd250e0ffdd9768b75f40218ad3a6b24d2f76
munenepeter <peternjega7@gmail.com>
2022-06-21 14:42:58 +0300

    Almost done with modal

--- app/Http/Livewire/PostIndex.php
+++ app/Http/Livewire/PostIndex.php
@@ -4,10 +4,15 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 
-class PostIndex extends Component
-{
-    public function render()
-    {
+class PostIndex extends Component {
+
+    public $showingPostModal = false;
+
+    public function showPostModal(){
+        $this->showingPostModal = true;
+    }
+
+    public function render() {
         return view('livewire.post-index');
     }
 }
--- resources/views/livewire/post-index.blade.php
+++ resources/views/livewire/post-index.blade.php
@@ -7,7 +7,7 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="flex justify-end m-2 p-2">
+            <div class="flex justify-end">
                 <x-jet-button wire:click="showPostModal">Create</x-jet-button>
             </div>
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
@@ -15,9 +15,29 @@
             </div>
             <div class="modal">
                 <x-jet-dialog-modal wire:model="showingPostModal">
-                    <x-slot name="title">Post Title</x-slot>
-                    <x-slot name="content">Post Content</x-slot>
-                    <x-slot name="footer">Post Footer</x-slot>
+                    <x-slot name="title">Create Post</x-slot>
+                    <x-slot name="content">
+
+
+                        <form method="post" action="">
+                           
+                            <div class="mb-6">
+                              <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Post Title</label>
+                              <input type="text"  name="title" id="title" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full h-14 p-4 " required="">
+                            </div>
+                            <div class="mb-6">
+                                <label for="body" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Post Body</label>
+                                <textarea id="body" name="body" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 " placeholder="Post Something..."></textarea>
+                            </div>
+                           
+                          </form>
+                          
+
+                    </x-slot>
+                    <x-slot name="footer">
+                        <x-jet-button class="bg-blue-500" wire:click="showPostModal">Save</x-jet-button>
+
+                    </x-slot>
                 </x-jet-dialog-modal>
 
             </div>
